63003f2e940448c1b1f401cfe4d56e3feb626218
JoBD <cbd902@naver.com>
2024-07-10 09:36:05 +0900

    [feature] 31

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -11,3 +11,6 @@ class PrimeFactorTest(TestCase):
 
     def test_prime_factor_of_2(self):
         self.assertEqual([2], self.prime_factor.calcPrime(2))
+
+    def test_prime_factor_of_3(self):
+        self.assertEqual([3], self.prime_factor.calcPrime(3))
